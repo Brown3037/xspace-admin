6f0a60917451cf70e0a62eecdcd7d3bfcbc74645
30373 <3037314881@qq.com>
2026-05-29 17:47:06 +0800

    chore: 初始化 X-Space Spring Boot 3 (JDK 17) 工程

---
+++ xspace-admin/src/main/java/com/xspace/xspace_admin/XspaceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.xspace.xspace_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XspaceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XspaceAdminApplication.class, args);
+	}
+
+}
---
+++ xspace-admin/src/test/java/com/xspace/xspace_admin/XspaceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xspace.xspace_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XspaceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
